8f77098fe0c27b57313525d8d17c1d5a20e93d23
asus <asus@ASUSLAPTOPHBA>
2021-01-23 12:09:43 +0300

    Add project files.

---
+++ Ders4Odev5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ders4Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+             
+            // Dictionary sınıfı kullanımı Dictionary<TKey,TValue> 
+            Dictionary<int, string> MyDictionary = new Dictionary<int, string>();
+
+            // key/value birlikte Add komutu ile eklenişi 
+            // Dictionary Add() methodu kullanımı 
+            MyDictionary.Add(123, "Yazılım");
+            MyDictionary.Add(124, "Geliştirici Yetiştirme");
+            MyDictionary.Add(125, "Kampı");
+
+            Console.WriteLine(MyDictionary[123]);
+            Console.WriteLine(MyDictionary[124]);
+            Console.WriteLine(MyDictionary[125]);
+
+                       
+
+        }
+    }
+}
